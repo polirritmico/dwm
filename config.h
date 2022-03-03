63ab230cbb048b38a6686270e8396c45bd43c51f
polirritmico <ebray187@gmail.com>
2022-03-02 22:37:42 -0300

    Fix Firefox classname in rules

--- config.h
+++ config.h
@@ -34,8 +34,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
