7e274fc9b93b2f43c253414c1388dca5b2c08202
polirritmico <ebray187@gmail.com>
2022-05-18 13:51:01 -0400

    Added screenoff shortcut and tweak code format

--- config.h
+++ config.h
@@ -85,48 +85,50 @@ static const char *changebg[] = { "feh", "--randomize", "--bg-fill",
 static const char *ayuda[]    = { "/usr/bin/urxvt", "-e", "bash", "-c",
                                   "/usr/bin/nvim /home/eduardo/Utilidades/comandos.md", 
                                   NULL };
+static const char *offscreen[] = { "/usr/local/bin/offscreen", NULL };
 
 /* keymaps */
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ ControlMask,                  XK_space,  spawn,          {.v = dmenucmd } },
-	{ Mod1Mask,                     XK_space,  spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ControlMask,           XK_j,      setcfact,       {.f = -0.25} },
-	{ MODKEY|ControlMask,           XK_k,      setcfact,       {.f = +0.25} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_x,      killclient,     {0} },
-	{ MODKEY,                       XK_n,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_f,      togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	//{ MODKEY|ShiftMask,             XK_o,      rotatestack,    {.i = -1 } },
-	//{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	//{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	//{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	//{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|Mod1Mask,              XK_i,      spawn,          {.v = browser } },
-	{ MODKEY|Mod1Mask,              XK_b,      spawn,          {.v = changebg } },
-    { MODKEY|Mod1Mask,              XK_h,      spawn,          {.v = ayuda } },
-    { MODKEY|Mod1Mask,              XK_e,      spawn,          {.v = editor } },
-    { MODKEY,                       XK_space,  spawn,          {.v = files } },
+	/* modifier                 key        function        argument */
+	{ ControlMask,              XK_space,  spawn,          {.v = dmenucmd } },
+	{ Mod1Mask,                 XK_space,  spawn,          {.v = termcmd } },
+	{ MODKEY,                   XK_b,      togglebar,      {0} },
+	{ MODKEY,                   XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                   XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                   XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                   XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                   XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                   XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ControlMask,       XK_j,      setcfact,       {.f = -0.25} },
+	{ MODKEY|ControlMask,       XK_k,      setcfact,       {.f = +0.25} },
+	{ MODKEY,                   XK_Return, zoom,           {0} },
+	{ MODKEY,                   XK_Tab,    view,           {0} },
+	{ MODKEY,                   XK_x,      killclient,     {0} },
+	{ MODKEY,                   XK_n,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                   XK_t,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,         XK_f,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                   XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                   XK_f,      togglefloating, {0} },
+	{ MODKEY,                   XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,         XK_0,      tag,            {.ui = ~0 } },
+	//{ MODKEY|ShiftMask,         XK_o,      rotatestack,    {.i = -1 } },
+	//{ MODKEY,                   XK_comma,  focusmon,       {.i = -1 } },
+	//{ MODKEY,                   XK_period, focusmon,       {.i = +1 } },
+	//{ MODKEY|ShiftMask,         XK_comma,  tagmon,         {.i = -1 } },
+	//{ MODKEY|ShiftMask,         XK_period, tagmon,         {.i = +1 } },
+	TAGKEYS(                    XK_1,                      0)
+	TAGKEYS(                    XK_2,                      1)
+	TAGKEYS(                    XK_3,                      2)
+	TAGKEYS(                    XK_4,                      3)
+	TAGKEYS(                    XK_5,                      4)
+	TAGKEYS(                    XK_6,                      5)
+	{ MODKEY|ShiftMask,         XK_q,      quit,           {0} },
+	{ MODKEY|Mod1Mask,          XK_i,      spawn,          {.v = browser   } },
+	{ MODKEY|Mod1Mask,          XK_b,      spawn,          {.v = changebg  } },
+    { MODKEY|Mod1Mask,          XK_h,      spawn,          {.v = ayuda     } },
+    { MODKEY|Mod1Mask,          XK_e,      spawn,          {.v = editor    } },
+    { MODKEY|Mod1Mask,          XK_0,      spawn,          {.v = offscreen } },
+    { MODKEY,                   XK_space,  spawn,          {.v = files     } },
 
 	{ 0, XF86XK_AudioMute,  spawn,
       SHCMD("pamixer -t; kill -41 $(pidof dwmblocks)") },
